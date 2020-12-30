b46146b391bcf27f46439ccb7b15831dcbed3a43
friendly-format-0074 <samuelagasi@gmail.com>
2020-12-30 22:41:37 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
-# Write your methods here
+def loop_message_five_times(message)
+  5.times {puts message}
+end
+loop_message_five_times("Hello World.")
+
+def loop_message_n_times(message, number)
+  number.times {puts message}
+end
+loop_message_n_times("Hello Moon.", 5)
+loop_message_n_times("Hello Red Ballon", 10)
+
+def output_array(array)
+    counter= 0
+
+    while counter< array.length do
+      puts array[counter]
+      counter+= 1
+    end
+end
+output_array(["Be yourself;", "everyone else is already taken", "- Oscar Wilde"])
+
+def return_string_array(array)
+  new_array= []
+  counter= 0
+
+  while counter< array.length do
+    element= array[counter]
+    counter+= 1
+    new_array.push(element.to_s)
+  end
+return new_array
+end
+return_string_array([5, 4, 3, 2, 1])
